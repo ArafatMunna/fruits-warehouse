ed41451a2a0cf5b5e96bdfcc6ccda04f9cde0056
ArafatMunna <arafatmunna000@gmail.com>
2022-04-30 05:03:40 +0600

    deliverd btn update added

--- src/Pages/Home/ItemUpdate/ItemUpdate.js
+++ src/Pages/Home/ItemUpdate/ItemUpdate.js
@@ -1,38 +1,54 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import './ItemUpdate.css';
 
 const ItemUpdate = () => {
     const { itemId } = useParams();
     const [item, setItem] = useState({});
-    const { name, img, _id, description, price, quantity, supplierName } = item;
+    const { name, img, description, price, quantity, supplierName } = item;
 
     useEffect(() => {
         axios
             .get(`http://localhost:5000/item/${itemId}`)
             .then((res) => setItem(res.data));
-    }, [itemId]);
+    }, [itemId, item]);
+
+    const updateItem = async (newQuantity) => {
+        await axios
+            .put(`http://localhost:5000/item/${itemId}`, { newQuantity })
+            .then((res) => {
+                setItem(res.data);
+                toast.success(res?.data?.message);
+            });
+    };
+
+    const handleDelivered = () => {
+        if (quantity > 0) {
+            const newQuantity = quantity - 1;
+            updateItem(newQuantity);
+        }
+    };
     return (
         <div className="container mt-5">
-            <div className="card">
-                <img
-                    height={250}
-                    src={img}
-                    className="card-img-top"
-                    alt="..."
-                />
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <p className="card-text">{description}</p>
-                    <p>Price: ${price}</p>
-                    <p>Quantity: {quantity}</p>
-                    <p>Supplier Name: {supplierName}</p>
-                    <button
-                        onClick={() => navigate(`/inventory/${_id}`)}
-                        className="btn btn-info"
-                    >
-                        Update
-                    </button>
+            <div className="mx-auto card-container">
+                <div className="card">
+                    <img src={img} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">{description}</p>
+                        <p>Price: ${price}</p>
+                        <p>Quantity: {quantity}</p>
+                        <p>Supplier Name: {supplierName}</p>
+                        {quantity === 0 ? <h5>Sold Out</h5> : ''}
+                        <button
+                            onClick={handleDelivered}
+                            className="btn btn-info"
+                        >
+                            Delivered
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
