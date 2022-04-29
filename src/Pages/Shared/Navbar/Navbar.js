0e065b2da325986698155bdf14a2e083660a45bc
ArafatMunna <arafatmunna000@gmail.com>
2022-04-29 17:45:24 +0600

    navbar added

---
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/blogs">
+                            Blogs
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/login">
+                            Login
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Navbar;
